739a06b0bc5d681478040551354e6f1409f8da86
Tki April <tki@tki.so>
2014-08-22 23:32:36 +0900

    PEP8 and docstring fix

--- wooglecalendar/app.py
+++ wooglecalendar/app.py
@@ -1,21 +1,27 @@
-""":mod:`woogle` --- Flask Backend for Woogle Calendar
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+""":mod:`wooglecalendar.app` --- Flask web app for Woogle Calendar
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 """
 
 from flask import Flask
+
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def calendar():
     return "Hello World!"
 
+
 @app.route('/schedule/<int:schedule_id>')
 def schedule(schedule_id):
     return "Schedule no. {}".format(schedule_id)
 
+
 def main():
     app.run()
+
 
 if __name__ == "__main__":
     main()
